3f552295b64891c1ebf8d0a44b599e5a7aee2f1e
Tony Cui <tonycui623@gmail.com>
2025-05-17 00:22:31 -0400

    Update p01.py

--- Tkinter GUI/M1/p01.py
+++ Tkinter GUI/M1/p01.py
@@ -1,1 +1,10 @@
-test
+import tkinter as tk  # Import tkinter module
+
+# Create the main window
+root = tk.Tk() #creates a root window.
+root.title("My First GUI") #sets the window title.
+root.geometry("300x200")  # sets the size. Width x Height
+
+# Start the GUI loop
+root.mainloop() #keeps the window open and responsive.
+
